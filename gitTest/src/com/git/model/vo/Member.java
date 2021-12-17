b306852a4f0f3e9a15ca077b5e5d7983e362ad17
Laptop <Laptop@192.168.219.101>
2021-12-17 10:51:07 +0900

    Member클래스에 생성자, get/set 추가 -ybs-

--- gitTest/src/com/git/model/vo/Member.java
+++ gitTest/src/com/git/model/vo/Member.java
@@ -4,4 +4,44 @@ public class Member {
 	private String name;
 	private int age;
 	private char gender;
+	
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Member(String name, int age, char gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	
 }
+
+
+
